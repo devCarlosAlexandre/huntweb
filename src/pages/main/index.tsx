cce991fb8cf0a795b3d75e98e39ca1b428996b69
devCarlosAlexandre <carloalexandresousa@gmail.com.br>
2022-06-15 18:44:57 -0300

    teste de commit

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -24,24 +24,7 @@ export default class Main extends Component {
 
     render() {
         const { products } = this.state;
-        return (< div className="product-list" > {
-            products.map(products => (<article >
-                < img src={products.image_link} > < /img>
-                    < h2 key={products.id} >
-                        {products.name}
-                        < /h2> <strong >
-                            {products.name}
-                            < /strong>
-                            < p >
-                                {products.description}
-                                < /p> <a > Para acessar basta clicar neste botão
-                                    < /a>
-                                </article>
-
-
-                                ))
-            } <div className="actions" >
-                                    <button onClick={this.prevPage} > ir ao proximo < /button> < button onClick={this.nextPage} > Ir ao anterior < /button> < /div>
-                                        );
+        () = () => ;
+        
     }
 }
